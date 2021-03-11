032d6bccda6fd8f46486b4c484d76a1eb34b253b
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-11 16:39:53 +0700

    2

--- 6320502991_5_2.c
+++ 6320502991_5_2.c
@@ -3,6 +3,14 @@ int main()
 {
     int n,i,j,x,X,m;
     scanf("%d",&n);
+    x=n/2;
+    if(n%2==0)
+        x--;
+    for(i=0;i<x;i++)
+        printf("_");
+    printf("*");
+    for(i=0;i<x;i++)
+        printf("_");
 
 
 
