ae2fbb00c14ec7811044bf4f5a06edbccef570d8
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-11 15:09:14 +0700

    2

--- 6320502991_5_2.c
+++ 6320502991_5_2.c
@@ -1,23 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,k;
+    int n,i,j,k,x;
     scanf("%d",&n);
     k=n/2;
     if(n%2==0)
       k--;
-    for(i=0;i<n;i++)
+    for(i=0;i<n/2;i++)
     {
-        for(j=1;j<=k;j++)
-        {
+        for(j=0;j<i;j++)
             printf("_");
-            if(j%2==0)
-                printf("*");
-            printf("_");
-        }
+        printf("*");
     printf("\n");
     }
-    printf("*");
 
 
 
