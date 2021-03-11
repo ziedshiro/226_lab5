f0eae3fbe0ca95a4c7fca8a2afd4f291cf71f9b8
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-11 15:40:15 +0700

    5

--- 6320502991_5_5.c
+++ 6320502991_5_5.c
@@ -1,7 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int n,m;
+    int n,m,i,j;
     scanf("%d",&n);
     scanf("%d",&m);
+    int x[n][n],X[m][m];
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+        scanf("%d",&x[i][j]);
+    for(i=0;i<n;i++)
+    {
+        {
+            for(j=i;j<m+i;j++)
+                printf("%d",x[i][j]);
+
+        }
+        printf("\n");
+    }
+
+
 }
