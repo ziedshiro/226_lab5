12248929e158fe750cd1df172016395f785a075d
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-11 15:17:59 +0700

    3

--- 6320502991_5_3.c
+++ 6320502991_5_3.c
@@ -1,12 +1,24 @@
 #include<stdio.h>
 int main()
 {
-    int n,i=0,k=0;
+    int n,i=0,k=0,z,x;
     scanf("%d",&n);
     do
     {
        k++;
-        if(k%2==0||k%3==0||k%5==0)
+       if(k>1)
+        {
+           z=k;
+           x=2;
+        do
+        {
+            if(z%x==0)
+                z/=x;
+            else
+                x++;
+        }while(z!=1);
+        }
+        if(k==1||x<=5)
             i++;
     }while(i!=n);
     printf("%d",k);
